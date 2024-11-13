244572154b84d0f982444496585a4cb73c5e6d6e
Nikolay Suvorov <90179432+nickVNDL@users.noreply.github.com>
2024-11-13 15:57:44 +0300

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -1,3 +1,6 @@
+# PowerShell скрипт для сбора информации о системных компонентах и их драйверах, включая видеокарту
+
+# Создаем объект для хранения информации
 $systemInfo = @{}
 
 # Получаем информацию о видеокарте
@@ -13,6 +16,20 @@ $systemInfo.VideoCard = @{
 $manufacturer = $videoCard.Manufacturer
 if ($manufacturer -eq "NVIDIA") {
     $url = "https://www.nvidia.com/Download/index.aspx?lang=en-us"
+    $response = Invoke-WebRequest -Uri $url
+    $html = $response.Content
+    $driverVersion = ($html | Select-String -Pattern "Version: (\d+\.\d+\.\d+)").Matches[0].Groups[1].Value
+    $systemInfo.VideoCard.NewDriverVersion = $driverVersion
+}
+elseif ($manufacturer -eq "Intel") {
+    $url = "https://downloadcenter.intel.com/product/$($videoCard.Name)"
+    $response = Invoke-WebRequest -Uri $url
+    $html = $response.Content
+    $driverVersion = ($html | Select-String -Pattern "Version: (\d+\.\d+\.\d+)").Matches[0].Groups[1].Value
+    $systemInfo.VideoCard.NewDriverVersion = $driverVersion
+}
+elseif ($manufacturer -eq "AMD") {
+    $url = "https://www.amd.com/en/support/kb/release-notes/rn-amdgpu-win-$($videoCard.Name)"
     $response = Invoke-WebRequest -Uri $url
     $html = $response.Content
     $driverVersion = ($html | Select-String -Pattern "Version: (\d+\.\d+\.\d+)").Matches[0].Groups[1].Value
